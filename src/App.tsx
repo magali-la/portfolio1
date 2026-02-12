b6fdfc401f10d2b33009ed4923bc9e2fc2a52e46
Mag <166319982+magali-la@users.noreply.github.com>
2026-02-12 11:21:09 -0500

    feat(nav): add navbar with active link styling, add custom tailwind header hierarchy, font, and color

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,25 @@ import NavBar from "./components/NavBar/NavBar"
 export default function App() {
   
   return (
-    <>
+    <div className="bg-pomegranate h-screen">
       <NavBar/>
       <h1>Hi I'm Magali.</h1>
-    </>
+      <h2>H2 header</h2>
+      <h3>H3 header</h3>
+      <h4>H4 header</h4>
+      <h5>H5 header</h5>
+      <h6>H6 header</h6>
+      <p>i'm normal text on the website</p>
+
+      {/* hero */}
+
+      {/* about section */}
+
+      {/* skills */}
+
+      {/* projects */}
+
+      {/* contact */}
+    </div>
   )
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,57 @@
-export default function NavBar() {
-    return (
-        <nav>
+import { useState } from "react"
 
+// interface for NavLink array
+interface NavLink {
+    id: string;
+    label: string;
+    href: string;
+}
+
+export default function NavBar() {
+    // set active tab in state for conditional styling
+    const [activeTab, setActiveTab] = useState<string>('');
+
+    // list of nav link objects - TODO: figure out if it's single page sections or swap of components for site nav
+    const navLinks: NavLink[] = [
+        {id: 'about', label: 'About', href: '#about'},
+        {id: 'skills', label: 'Skills', href: '#skills'},
+        {id: 'projects', label: 'Projects', href: '#projects'},
+        {id: 'contact', label: 'Contact', href: '#contact'}
+    ]
+
+    function handleActiveTab(tabId: string) {
+        console.log('Active tab clicked', tabId);
+        // set the active tab for styling purposes
+        setActiveTab(tabId);
+    }
+
+    function handleHomeClick() {
+        // reset it so no tabs show
+        setActiveTab('');
+    }
+
+    return (
+        <nav className="md:h-[10vh] px-4 bg-amber-50 flex flex-col gap-4 md:flex-row items-start md:items-end justify-between" role="navigation">
+            {/* name section */}
+            <div className="text-nowrap self-center">
+                <a href="#" className="text-pomegranate text-4xl" onClick={handleHomeClick}>Magali Lapu</a>
+            </div>
+
+            {/* nav links mapped */}
+            <div className="grid grid-cols-4 gap-0 h-full items-end w-full md:w-[75%]" role="navigation" aria-label="navigation links">
+                {navLinks.map((item) => (
+                    <a href={item.href} key={item.id} className="nav-link" onClick={() => handleActiveTab(`#${item.id}`)} aria-current={activeTab === `#${item.id}` ? 'page' : undefined}>
+                        <div className={`flex justify-center pb-1 md:pb-2 rounded-t-lg text-lg md:text-2xl h-[50%] transition-all duration-200
+                            ${activeTab === `#${item.id}`
+                                ? 'bg-pomegranate text-white font-medium italic pt-2'
+                                : 'hover:bg-pomegranate/20 hover:pt-0.5'
+                            }
+                        `}>
+                            {item.label}
+                        </div>
+                    </a>
+                ))}
+            </div>
         </nav>
     )
 }
